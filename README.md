849042226037d22bddc21dce3d9be8779d914bfe
Joshua <joshmack02@gmail.com>
2023-01-31 14:05:25 -0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # pet-adoption
+
 A pet adoption app built using React
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
 import { createRoot } from "react-dom/client";
-import Pet from "./Pet";
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   return (
-    <div>
-    <h1>Adopt Me!</h1>
-    <Pet name="Luna" animal="dog" breed="Havanese" />
-    <Pet name="Pepper" animal="bird" breed="cockatoo" />
-    <Pet name="Doink" animal="cat" breed="Mixed" />
-  </div>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+      <h1>Adopt Me!</h1>
+       <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </QueryClientProvider>
+    </BrowserRouter>
   );
 };
 
